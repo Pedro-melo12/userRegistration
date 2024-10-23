5db4227e372de41abd4e6bb3bc13a2d519e4f8a9
Yuri Ernani <yuriernania@gmail.com>
2024-10-23 11:03:29 -0300

    First Commit

--- userRegistration/src/main/java/userRegistration/UserRegistrationApplication.java
+++ userRegistration/src/main/java/userRegistration/UserRegistrationApplication.java
@@ -2,12 +2,18 @@ package userRegistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class UserRegistrationApplication {
+public class UserRegistrationApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserRegistrationApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		System.out.println("Oi Peu");
 	}
 
 }
